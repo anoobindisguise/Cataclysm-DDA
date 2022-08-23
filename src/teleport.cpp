690f1325306e96e17de00dda1caadeccf0967a7d
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-08-23 10:48:33 -0700

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/teleport.cpp
+++ src/teleport.cpp
@@ -143,11 +143,12 @@ bool teleport::teleport_to_point( Creature &critter, tripoint target, bool safe,
             const bool poor_soul_is_u = poor_soul->is_avatar();
             bool frag = false;
             //decide whether to frag the target. if it's not the player it's a coinflip. the player is immune unless they have teleglow in which case it's also a coinflip.
-            if ( ( one_in( 2 ) && !poor_soul_is_u ) ) || ( poor_soul_is_u && poor_soul->has_effect( effect_teleglow ) && one_in( 2 ) ) ) {
+            if( ( one_in( 2 ) && !poor_soul_is_u ) ) || ( poor_soul_is_u &&
+                        poor_soul->has_effect( effect_teleglow ) && one_in( 2 ) ) ) {
                 frag = true;
-            }            
+            }
             if( frag ) {
-                if( poor_soul_is_u && display_message ) {
+            if( poor_soul_is_u && display_message ) {
                     add_msg( m_bad, _( "…" ) );
                     add_msg( m_bad, _( "You explode into thousands of fragments." ) );
                 }
@@ -191,10 +192,10 @@ bool teleport::teleport_to_point( Creature &critter, tripoint target, bool safe,
                 }
                 //teleport the victim out of the way instead of them being telefragged.
                 poor_soul->teleport():
+                }
             }
         }
-    }
-    critter.setpos( target );
+        critter.setpos( target );
     //player and npc exclusive teleporting effects
     if( p ) {
         g->place_player( p->pos() );
